96200241d8d56e919b193eebd6b720ab4c15e5a8
dev03lkomavyax <149859737+dev03lkomavyax@users.noreply.github.com>
2023-12-06 13:03:57 +0530

    added about us,contact us, privacy cms and bookigs route

---
+++ src/pages/AdminPanel/bookings/Bookings.js
@@ -0,0 +1,12 @@
+import Wrapper from '../../Wrapper'
+import classes from './Bookings.module.css'
+
+const Bookings = () => {
+    return (
+        <Wrapper>
+            <h1>Bookings</h1>
+        </Wrapper>
+    )
+}
+
+export default Bookings
